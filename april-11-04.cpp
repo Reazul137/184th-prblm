cfb947ab4a04093150fc71599a4fa6d4a31505df
Reazul Islam <reazulislam137@gmail.com>
2018-04-11 01:43:03 +0600

    first commit

---
+++ april-11-04.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float ar, s, n;
+
+    cout << "\n\n Print the area of a polygon : \n";
+    cout << "-----------------------------------\n";
+
+    cout << " Input the number of sides of the polygon : ";
+    cin >> n;
+
+    cout << " Input the length of each sides of the polygon : ";
+    cin >> s;
+
+    ar= (n * (s * s)) / (4.0 * tan((M_PI / n)));
+
+    cout <<" The area of the ploygon is : "<<ar<<"\n";
+    cout << endl;
+    return 0;
+}
+
